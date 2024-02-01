8292cc2c55b2b9315be229eddee771e7a802f3fd
KislyukAnna <kis-annet@mail.ru>
2024-02-01 13:36:19 +0800

    add Lesson2:task4

---
+++ Lesson2/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 5;
+//int[] arr = new int[10]{2,6,5,3,4,8,9,8,3,10};
+int[] array = { 10, 4, 7, 3, 9 };
+
+int i = 0;
+int max = array[0];
+
+while (i < n) //(i < arr.Lenght)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+
+    i = i + 1;
+}
+System.Console.Write($"{max}");
