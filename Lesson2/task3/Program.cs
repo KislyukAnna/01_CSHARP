f50737a2be0b982133ed5f8c773e5c71fc534c13
KislyukAnna <kis-annet@mail.ru>
2024-02-01 13:27:12 +0800

    add Lesson2:task3

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 10;
+//int[] arr = new int[10]{2,6,5,3,4,8,9,8,3,10};
+int[] array = {2,6,5,3,4,8,9,8,3,10};
+
+int i = 0;
+
+while (i<n) //(i < arr.Lenght)
+{
+    if (array[i] % 2 == 0)
+    {
+       System.Console.Write($"{array[i]} "); 
+    }
+    
+    i = i + 1;
+}
