6ad8e6ebddf24a796433de57ddc2451665aed9df
KislyukAnna <kis-annet@mail.ru>
2024-02-05 13:01:45 +0800

    add Lesson3:task3

---
+++ Lesson3/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 4, 2, 10, 4, -8 };
+ZeroEvenElements(array);
+PrintArray(array);
