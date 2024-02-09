e20d0b75c7927a899cee75690eea5f544c39774d
KislyukAnna <kis-annet@mail.ru>
2024-02-10 01:38:18 +0800

    add Lesson4:task3

---
+++ Lesson4/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// функция выделения из строки буквенных символов
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine()!;
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
